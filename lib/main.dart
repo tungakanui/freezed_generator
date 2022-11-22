d25671551bebc4fa4635044c4d4bc6046104a2fe
tungakanui <tung.ngthanh99@gmail.com>
2022-11-22 15:57:07 +0700

    remove final on constructor

--- lib/main.dart
+++ lib/main.dart
@@ -71,34 +71,34 @@ void fromJsonToObject(Map<String, dynamic> json, String className) {
       if (key.contains('_') || key.startsWith(r"$")) {
         fields += '@JsonKey(name: "$key") ';
       }
-      fields += 'final dynamic ${key.replaceAll("\$", "").camelCase},\n';
+      fields += 'dynamic ${key.replaceAll("\$", "").camelCase},\n';
     } else if (json[key] is String) {
       if (key.contains('_') || key.startsWith(r"$")) {
         fields += '@JsonKey(name: "$key") ';
       }
-      fields += 'final String? ${key.replaceAll("\$", "").camelCase},\n';
+      fields += 'String? ${key.replaceAll("\$", "").camelCase},\n';
     } else if (json[key] is int) {
       if (key.contains('_') || key.startsWith(r"$")) {
         fields += '@JsonKey(name: "$key") ';
       }
-      fields += 'final int? ${key.replaceAll("\$", "").camelCase},\n';
+      fields += 'int? ${key.replaceAll("\$", "").camelCase},\n';
     } else if (json[key] is double) {
       if (key.contains('_') || key.startsWith(r"$")) {
         fields += '@JsonKey(name: "$key") ';
       }
-      fields += 'final double? ${key.replaceAll("\$", "").camelCase},\n';
+      fields += 'double? ${key.replaceAll("\$", "").camelCase},\n';
     } else if (json[key] is bool) {
       if (key.contains('_') || key.startsWith(r"$")) {
         fields += '@JsonKey(name: "$key") ';
       }
-      fields += 'final bool? ${key.replaceAll("\$", "").camelCase},\n';
+      fields += 'bool? ${key.replaceAll("\$", "").camelCase},\n';
     } else if (json[key] is Map) {
       final name = getNameIfExist(key);
       if (key.contains('_') || key.startsWith(r"$")) {
         fields +=
-            '@JsonKey(name: "$key") final $name? ${key.replaceAll("\$", "").camelCase},\n';
+            '@JsonKey(name: "$key") $name? ${key.replaceAll("\$", "").camelCase},\n';
       } else {
-        fields += 'final $name? ${key.replaceAll("\$", "").camelCase},\n';
+        fields += '$name? ${key.replaceAll("\$", "").camelCase},\n';
       }
       classNames.add(name);
       fromJsonToObject(json[key], name);
@@ -106,58 +106,53 @@ void fromJsonToObject(Map<String, dynamic> json, String className) {
       if ((json[key] as List).isEmpty) {
         if (key.contains('_') || key.startsWith(r"$")) {
           fields +=
-              '@JsonKey(name: "$key") final List<dynamic>? ${key.replaceAll("\$", "").camelCase},\n';
+              '@JsonKey(name: "$key") List<dynamic>? ${key.replaceAll("\$", "").camelCase},\n';
         } else {
-          fields +=
-              'final List<dynamic>? ${key.replaceAll("\$", "").camelCase},\n';
+          fields += 'List<dynamic>? ${key.replaceAll("\$", "").camelCase},\n';
         }
       } else if (json[key].first is String) {
         if (key.contains('_') || key.startsWith(r"$")) {
           fields +=
-              '@JsonKey(name: "$key") final List<String>? ${key.replaceAll("\$", "").camelCase},\n';
+              '@JsonKey(name: "$key") List<String>? ${key.replaceAll("\$", "").camelCase},\n';
         } else {
-          fields +=
-              'final List<String>? ${key.replaceAll("\$", "").camelCase},\n';
+          fields += 'List<String>? ${key.replaceAll("\$", "").camelCase},\n';
         }
       } else if (json[key].first is int) {
         if (key.contains('_') || key.startsWith(r"$")) {
           fields +=
-              '@JsonKey(name: "$key") final List<int>? ${key.replaceAll("\$", "").camelCase},\n';
+              '@JsonKey(name: "$key") List<int>? ${key.replaceAll("\$", "").camelCase},\n';
         } else {
-          fields += 'final List<int>? ${key.replaceAll("\$", "").camelCase},\n';
+          fields += 'List<int>? ${key.replaceAll("\$", "").camelCase},\n';
         }
       } else if (json[key].first is double) {
         if (key.contains('_') || key.startsWith(r"$")) {
           fields +=
-              '@JsonKey(name: "$key") final List<double>? ${key.replaceAll("\$", "").camelCase},\n';
+              '@JsonKey(name: "$key") List<double>? ${key.replaceAll("\$", "").camelCase},\n';
         } else {
-          fields +=
-              'final List<double>? ${key.replaceAll("\$", "").camelCase},\n';
+          fields += 'List<double>? ${key.replaceAll("\$", "").camelCase},\n';
         }
       } else if (json[key].first is bool) {
         if (key.contains('_') || key.startsWith(r"$")) {
           fields +=
-              '@JsonKey(name: "$key") final List<bool>? ${key.replaceAll("\$", "").camelCase},\n';
+              '@JsonKey(name: "$key") List<bool>? ${key.replaceAll("\$", "").camelCase},\n';
         } else {
-          fields +=
-              'final List<bool>? ${key.replaceAll("\$", "").camelCase},\n';
+          fields += 'List<bool>? ${key.replaceAll("\$", "").camelCase},\n';
         }
       } else if (json[key].first is List) {
         if (key.contains('_') || key.startsWith(r"$")) {
           fields +=
-              '@JsonKey(name: "$key") final List<List<dynamic>>? ${key.replaceAll("\$", "").camelCase},\n';
+              '@JsonKey(name: "$key") List<List<dynamic>>? ${key.replaceAll("\$", "").camelCase},\n';
         } else {
           fields +=
-              'final List<List<dynamic>>? ${key.replaceAll("\$", "").camelCase},\n';
+              'List<List<dynamic>>? ${key.replaceAll("\$", "").camelCase},\n';
         }
       } else if (json[key].first is Map) {
         final name = getNameIfExist(key.camelCase.titleCase);
         if (key.contains('_') || key.startsWith(r"$")) {
           fields +=
-              '@JsonKey(name: "$key") final List<$name>? ${key.replaceAll("\$", "").camelCase},\n';
+              '@JsonKey(name: "$key") List<$name>? ${key.replaceAll("\$", "").camelCase},\n';
         } else {
-          fields +=
-              'final List<$name>? ${key.replaceAll("\$", "").camelCase},\n';
+          fields += 'List<$name>? ${key.replaceAll("\$", "").camelCase},\n';
         }
         classNames.add(name);
         final List data = json[key] as List<dynamic>;
